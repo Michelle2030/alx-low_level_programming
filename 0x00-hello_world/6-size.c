b333709aa68a5417cb98ed405153476696e4ccfb
Michelle <michellekaihuri@gmail.com>
2022-12-12 13:28:40 -0800

    ws

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("size of a char:%i byt(s)\n", sizeof(char));
+	printf("size of an int:%i byt(s)\n", sizeof(int));
+	printf("size of a long int:%i byt(s)\n", sizeof(long int));
+	printf("size of a long long int:%i byt(s)\n", sizeof(long long int));
+	printf("size of an float:%i byt(s)\n", sizeof(float));
+	return (0);
+}
